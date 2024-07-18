f8977ab61735b2821f48eb91e21411770e57cb0a
sebastian <sebastian.oyarzo.zapata@gmail.com>
2024-07-17 20:23:10 -0400

    ajustes

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,19 @@
 // })
 
 
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   build: {
+//     outDir: 'build', // Directorio de salida personalizado
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    outDir: 'build', // Directorio de salida personalizado
-  },
+  base: '/sebastian-oyarzo.github.io/',
+  plugins: [react()],
 });
