ff9e7ea530b30286a45b0bcadd55b456074dff68
sebastian <sebastian.oyarzo.zapata@gmail.com>
2024-07-17 22:10:30 -0400

    ajustes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,66 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter , createHashRouter , RouterProvider } from 'react-router-dom'
 import ContextProvider from './Context.jsx'
+import { Home } from './views/Home'
+import { NavdarPizza } from './components/NavdarPizza'
+import { Carrito } from './views/Carrito'
+import { HomePizza } from './views/HomePizza'
+import { DetallePizza } from './views/DetallePizza'
+import { Navigate } from 'react-router'
+
+const router = createHashRouter([
+  {
+    path:"/",
+    element: <Home /> ,
+  },
+  {
+    path:"/homepizza",
+    element:
+      <div>
+      <NavdarPizza />
+      <HomePizza />
+      </div>
+  },
+  {
+    path:"/pizza/:id" ,
+    element:
+      <div>
+        <NavdarPizza />
+        <DetallePizza />
+      </div>
+  },
+  {
+    path:"/pizza/:*",
+     element:
+      <div>
+           <NavdarPizza />
+           <Navigate to="/" />
+      </div>
+  },
+  {
+    path:"/carrito",
+    element:
+      <div>
+        <NavdarPizza />
+        <Carrito />
+      </div>
+  } ,
+  {
+    path:"*" ,
+    element:<Navigate to="/" />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ContextProvider>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
+      <RouterProvider router={router}>
+        <BrowserRouter>
+          <App/>
+        </BrowserRouter>
+      </RouterProvider>
     </ContextProvider>
   </React.StrictMode>
 )
